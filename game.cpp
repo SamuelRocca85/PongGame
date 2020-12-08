26851780560937bb5ea8c33b6260f319ff8e8ae2
Samuel Rocha <samuelandres_rocha@unitec.edu>
2020-12-08 16:29:55 -0600

    M

--- game.cpp
+++ game.cpp
@@ -3,112 +3,38 @@
 #include <stdlib.h>
 using namespace std;
 
-//#define waitkey rlutil::anykey("Presiona cualquier tecla para continuar...\n")
 #define startkey rlutil::anykey("Pulse cualquier tecla para comenzar...\n\n")
-#define mapC 28
-#define mapF 10
-
-struct object
-{
-    int x;
-    int y;
-    char val;
-};
 
 
-void showMap(int m[mapF][mapC]);
+void drawMap();
 
 int main()
 {
     rlutil::cls();
-    int map[mapF][mapC] = 
-    {1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1,
-     1 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,2 ,0 ,3 ,0 ,2 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,1,
-     1 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,1,
-     1 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,3 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,1,
-     1 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,4 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,1,
-     1 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,3 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,1,
-     1 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,1,
-     1 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,3 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,1,
-     1 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,1,
-     1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1};
-    
-    //rlutil::hidecursor();
-    int option = 0;
-
-    do{
-        rlutil::showcursor();
-        rlutil::setColor(5);
-        cout <<"    * * * PONG * * *\n";
-        rlutil::setColor(15);
-        cout << "1. Jugar\t2. Salir" << endl << "Seleccione la opcion que desea: ";
-        cin >> option;
-
-        switch (option)
-        {
-        case 1:
-            rlutil::setColor(10); 
-            showMap(map);
-            startkey;
-            break;
-        case 2:
-            exit(0);
-            break;
-        }
-
-    }while(option != 2);
-    
-    
-    
-    rlutil::cls();
-    rlutil::showcursor();
-
+    drawMap();
+    startkey;
 }
 
-void showMap(int m[mapF][mapC])
+void drawMap()
 {
-    object ball; ball.val = 'o';
-    rlutil::hidecursor();
-    rlutil::cls();
-    rlutil::locate(8,0); cout << "J1";
-    rlutil::locate(20,0); cout << "J2";
-    int x = 0, y = 2;
-    for(int i = 0; i < mapF;i++,y++)
+    for(int i = 2; i < 60; i++)
     {
-        rlutil::locate(x,y);
-        for(int j = 0; j < mapC;j++,x++)
-        {
-            switch(m[i][j])
-            {
-                case 0: cout << " "; break;
-                case 1: cout << "#"; break;
-                case 2: cout << "0"; break;
-                case 3:
-                    rlutil::setColor(15); 
-                    cout << "|"; 
-                    rlutil::setColor(10);
-                    break;
-                case 4:
-                    ball.x = x; ball.y = y; rlutil::locate(ball.x+ 1 , ball.y);
-                    cout << ball.val;
-                    rlutil::setColor(9); 
-                    ball.val = 'o';
-                    rlutil::setColor(10); 
-                    break;
-            }
-        }
-        x = 0;
-    }
-    cout << endl << endl << endl;
+        gotoxy(i, 1);
+        cout << "#";
 
-    while(ball.x > 2)
-    {
-        gotoxy(ball.x,ball.y); std::cout << " ";
-        ball.x--;
-        gotoxy(ball.x , ball.y);
-        cout << ball.val;
+        gotoxy(i, 24);
+        cout << "#";
     }
 
-    rlutil::locate(0,14);
-    cout.flush();
+    for (int i =2; i < 24; i++)
+    {
+        gotoxy(1,i);
+        cout << "#";
+
+        gotoxy(60,i);
+        cout << "#";
+    }
+
+    gotoxy(62,12);
 }
+
